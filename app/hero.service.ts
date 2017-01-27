84469ca0efabca0043bb8193f157918088924e31
Pedro Pires <pedro.pires@accenture.com>
2017-01-27 09:59:09 +0000

    Finished navigation ... Still need to style the app to finish the chapter on routing ...

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -11,11 +11,16 @@ export class HeroService {
   }
 
   getHeroesSlowly(): Promise<Hero[]> {
-  return new Promise(resolve => {
-    // Simulate server latency with 2 second delay
-    setTimeout(() => resolve(this.getHeroes()), 2000);
-  });
-}
+    return new Promise(resolve => {
+      // Simulate server latency with 2 second delay
+      setTimeout(() => resolve(this.getHeroes()), 2000);
+    });
+  }
+
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes()
+              .then(heroes => heroes.find(hero => hero.id === id));
+  }
 
 
 }
